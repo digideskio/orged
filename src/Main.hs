a869ab5cc14d00f4fb592750ea934e132dd57c83
Daniel Patterson <dbp@dbpmail.net>
2015-05-14 21:25:54 -0400

    Implement Top3/Done strategy, and add labels, subscription, and urls to cards.
    Subscription is used to make sure we don't step on manually created
    cards toes.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Prelude             hiding (concatMap, find, mapM, mapM_)
+import           Prelude             hiding (concatMap, elem, find, mapM, mapM_)
 
 import           Control.Applicative
 import           Control.Lens
@@ -9,6 +9,7 @@ import           Data.Aeson.Lens
 import           Data.Foldable
 import           Data.Map            (Map)
 import qualified Data.Map            as M
+import           Data.Maybe          (catMaybes)
 import           Data.Monoid
 import           Data.Set            (Set)
 import qualified Data.Set            as S
@@ -39,6 +40,9 @@ data Card = Card { cardId          :: CardId
                  , cardName        :: Text
                  , cardDescription :: Text
                  , cardCheckList   :: Maybe (Set Text)
+                 , cardSubscribed  :: Bool
+                 , cardLabels      :: [Text]
+                 , cardUrl         :: Text
                  } deriving (Eq, Show, Ord)
 
 newtype ListId = ListId { unListId :: Text } deriving (Eq, Show, Ord)
@@ -77,6 +81,14 @@ checklistEndpoint :: Text -> Text
 checklistEndpoint i = "https://api.trello.com/1/checklists/" <> i
 cardEndpoint :: CardId -> Text
 cardEndpoint (CardId i) = "https://api.trello.com/1/cards/" <> i
+cardSubscribedEndpoint :: CardId -> Text
+cardSubscribedEndpoint i = cardEndpoint i <> "/subscribed"
+cardLabelsEndpoint :: CardId -> Text
+cardLabelsEndpoint i = cardEndpoint i <> "/labels"
+cardDescEndpoint :: CardId -> Text
+cardDescEndpoint i = cardEndpoint i <> "/desc"
+userBoardsEndpoint :: Text -> Text
+userBoardsEndpoint userId = "https://api.trello.com/1/members/" <> userId <> "/boards"
 
 getChecklist :: Creds -> Text -> IO [Text]
 getChecklist creds checklistId =
@@ -95,7 +107,10 @@ getCards creds listId =
                 return $ Card (CardId (v ^?! key "id" . _String))
                               (v ^?! key "name" . _String)
                               (v ^?! key "desc" . _String)
-                              checklist)
+                              checklist
+                              (v ^?! key "subscribed" . _Bool)
+                              (v ^.. key "labels" . values . key "color" . _String)
+                              (v ^?! key "shortUrl" . _String))
           (r ^.. responseBody . values)
 
 getLists :: Creds -> BoardId -> IO [List]
@@ -109,8 +124,22 @@ getLists creds boardId =
                                   )
                   (r ^.. responseBody . values)
 
-getBoards :: Creds -> Text -> IO [Board]
-getBoards creds orgName =
+notClosed v = not $ v ^?! key "closed" . _Bool
+
+getUserBoards :: Creds -> Text -> IO [Board]
+getUserBoards creds userId =
+  do r <- get' creds (userBoardsEndpoint userId)
+     mapM (\v ->
+               do let boardId = BoardId (v ^?! key "id" . _String)
+                  lists <- S.fromList <$> getLists creds boardId
+                  return $ Board boardId
+                                 (v ^?! key "name" . _String)
+                                 lists
+               )
+               (filter notClosed (r ^.. responseBody . values))
+
+getOrgBoards :: Creds -> Text -> IO [Board]
+getOrgBoards creds orgName =
   do r <- get' creds (groupEndpoint orgName)
      mapM (\v ->
                do let boardId = BoardId (v ^?! key "id" . _String)
@@ -119,7 +148,7 @@ getBoards creds orgName =
                                  (v ^?! key "name" . _String)
                                  lists
                )
-               (r ^.. responseBody . values)
+               (filter notClosed (r ^.. responseBody . values))
 
 
 addList :: Creds -> BoardId -> Text -> IO ListId
@@ -131,23 +160,37 @@ archiveList :: Creds -> ListId -> IO ()
 archiveList creds list = do put' creds (listClosedEndpoint list) [("value", "true")]
                             return ()
 
-addCard :: Creds -> ListId -> Text -> Text -> IO CardId
-addCard creds listId name desc =
+setCardSubscribed :: Creds -> CardId -> Bool -> IO ()
+setCardSubscribed creds card subscribe =
+  do put' creds (cardSubscribedEndpoint card) [("value", case subscribe of
+                                                           True -> "true"
+                                                           False -> "false")]
+     return ()
+
+setCardLabels :: Creds -> CardId -> [Text] -> IO ()
+setCardLabels creds card labels =
+  do put' creds (cardLabelsEndpoint card) [("value", T.intercalate "," labels)]
+     return ()
+
+setCardDesc :: Creds -> CardId -> Text -> IO ()
+setCardDesc creds card desc =
+  do put' creds (cardDescEndpoint card) [("value", desc)]
+     return ()
+
+addCard :: Creds -> ListId -> Text -> Text -> Bool -> [Text] -> IO CardId
+addCard creds listId name desc subscribe labels =
   do r <- post' creds (listCardsEndpoint listId)
-                      [("name", name),("due", "null"),("desc", desc)]
-     return $ CardId $ r ^?! responseBody . key "id" . _String
+                      (addLabels [("name", name),("due", "null"),("desc", desc)])
+     let cardId = CardId $ r ^?! responseBody . key "id" . _String
+     setCardSubscribed creds cardId subscribe
+     return cardId
+  where addLabels rest = if null labels
+                            then rest
+                            else ("labels", T.intercalate "," labels) : rest
 
 deleteCard :: Creds -> CardId -> IO ()
 deleteCard creds card = do delete' creds (cardEndpoint card)
                            return ()
-
-
-mainBoard = BoardId "55528aa166fe462917fc9dc0"
-
-projects =
-  [(BoardId "5506e72021961efe09fa5ca4", "Haymarket", "https://trello.com/b/DhK1G0jg/haymarket")
-  ,(BoardId "54d3f234c4aa2e743cb57534", "Verso", "https://trello.com/b/ooVAX4gj/verso")
-  ,(BoardId "54c99a52dd268d7dc428c586", "TNI", "https://trello.com/b/2DEBPclN/tni")]
 
 notColonPrefixed :: Text -> Bool
 notColonPrefixed s = not $ T.isPrefixOf ":" s
@@ -156,19 +199,32 @@ data Change = AddList BoardId List
             | RemoveList BoardId ListId
             | AddCard ListId Card
             | RemoveCard ListId CardId
+            | SetCardDesc CardId Text
+            | SetCardLabels CardId [Text]
+            | SetCardSubscribed CardId Bool
+     deriving (Eq, Show, Ord)
 
 diffCards :: ListId -> Set Card -> Set Card -> [Change]
-diffCards l old new = concatMap (\o -> case find (\n -> cardName o == cardName n) new of
-                                         Nothing -> [RemoveCard l (cardId o)]
-                                         Just n ->
-                                           if o == n
-                                             then []
-                                             else error $ "Don't support diffing cards yet: "
-                                                        <> show o <> " to " <> show n)
-                                old
-                   <> concatMap (\n -> case find (\o -> cardName o == cardName n) old of
-                                         Nothing -> [AddCard l n]
-                                         Just _ -> []) new
+diffCards l old new =
+  concatMap (\o -> case find (\n -> cardName o == cardName n) new of
+                     Nothing -> [RemoveCard l (cardId o)]
+                     Just n ->
+                       catMaybes
+                          [if cardLabels o /= cardLabels n
+                            then Just $ SetCardLabels (cardId o) (cardLabels n)
+                            else Nothing,
+                          if cardDescription o /= cardDescription n
+                           then Just $ SetCardDesc (cardId o) (cardDescription n)
+                           else Nothing,
+                          if cardSubscribed o /= cardSubscribed n
+                           then Just $ SetCardSubscribed (cardId o) (cardSubscribed n)
+                           else Nothing
+                          ]
+                       )
+            old
+  <> concatMap (\n -> case find (\o -> cardName o == cardName n) old of
+                        Nothing -> [AddCard l n]
+                        Just _ -> []) new
 
 diffLists :: BoardId -> Set List -> Set List -> [Change]
 diffLists b old new = concatMap (\o -> case find (\n -> listName o == listName n) new of
@@ -183,35 +239,67 @@ diffLists b old new = concatMap (\o -> case find (\n -> listName o == listName n
 effectChange :: Creds -> Change -> IO ()
 effectChange creds (AddList board list) =
   do lid <- addList creds board (listName list)
-     mapM_ (\c -> addCard creds lid (cardName c) (cardDescription c))
+     mapM_ (effectChange creds . (AddCard (listId list)))
            (S.toList (listCards list))
 effectChange creds (RemoveList board list) = archiveList creds list
 effectChange creds (AddCard list card) =
-  do addCard creds list (cardName card) (cardDescription card)
+  do addCard creds list (cardName card) (cardDescription card) (cardSubscribed card) (cardLabels card)
      return ()
 effectChange creds (RemoveCard list card) = deleteCard creds card
+effectChange creds (SetCardDesc card desc) = setCardDesc creds card desc
+effectChange creds (SetCardLabels card labels) = setCardLabels creds card labels
+effectChange creds (SetCardSubscribed card subscribed) = setCardSubscribed creds card subscribed
+
+getOrgedUserBoards creds = do userId <- T.pack <$> getEnv "TRELLO_USER_ID"
+                              getUserBoards creds userId
+
+top3DoneStrategy :: Board -> [Board] -> Board
+top3DoneStrategy summaryBoard projectBoards =
+  do let addPrefix p c = c { cardName = p <> cardName c }
+     let addDoneLabel c = case find (== "green") (cardLabels c) of
+                            Nothing -> c { cardLabels = "green" : cardLabels c}
+                            Just _ -> c
+     let customize c = c { cardSubscribed = True, cardDescription = cardUrl c, cardLabels = [] }
+     let findAll name = concatMap (\b -> case find (\l -> listName l == name)
+                                                   (boardLists b) of
+                                    Nothing -> []
+                                    Just l -> map (addPrefix (boardName b <> ": "))
+                                                  (S.toList (listCards l)))
+                            projectBoards
+     let top3 = findAll "Top 3"
+     let done = findAll "Done"
+     let allOurs = top3 <> done
+     let allExisting = S.fromList $
+                         concatMap (\l -> map cardName $ S.toList $ listCards l)
+                                   (boardLists summaryBoard)
+     let unscheduled =
+           S.fromList $ filter (\c -> not $ S.member (cardName c) allExisting)
+                               (map customize allOurs)
+     let updateCard c = case find ((== cardName c) . cardName) done of
+                          Nothing -> c
+                          Just _ -> addDoneLabel c
+     let permittedCard c = case cardSubscribed c of
+                             True -> case find ((== cardName c) . cardName) allOurs of
+                                       Nothing -> False
+                                       Just _ -> True
+                             False -> True
+     let newLists = S.map (\l ->
+           let newList = if listName l == "Unscheduled"
+                            then l { listCards = S.union unscheduled (listCards l)}
+                            else l in
+           newList { listCards = S.filter permittedCard $
+                                 S.map updateCard (listCards newList)})
+                                                  (boardLists summaryBoard)
+     summaryBoard { boardLists = newLists }
 
 main :: IO ()
 main = do readDotEnv
           creds <- readCreds
-          projectBoards <- mapM (\(i, name, _) -> do lists <- S.fromList <$> getLists creds i
-                                                     return (Board i name lists))
-                                               projects
-          projectLists <- getLists creds mainBoard
+          allBoards <- getOrgedUserBoards creds
+          let [summaryBoard] = filter ((== "Position Experiment") . boardName) allBoards
+          let projectBoards = filter ((/= "Position Experiment") . boardName) allBoards
 
-          let addPrefix p c = c { cardName = p <> cardName c }
-          let cards = concatMap (\b -> case find (\l -> listName l == "Top 3")
-                                                 (boardLists b) of
-                                  Nothing -> []
-                                  Just l -> map (addPrefix (boardName b <> ": "))
-                                               (S.toList (listCards l)))
-                          projectBoards
-          let allExisting = S.fromList $
-                              concatMap (\l -> map cardName $ S.toList $ listCards l)
-                                        projectLists
-          let newCards = S.fromList $ filter (\c -> not $ S.member (cardName c) allExisting) cards
-          let newLists = S.fromList $ map (\l -> if listName l == "Later"
-                                                    then l { listCards = S.union newCards (listCards l)}
-                                                    else l) projectLists
-          let changes = diffLists mainBoard (S.fromList projectLists) newLists
+          let newSummary = top3DoneStrategy summaryBoard projectBoards
+
+          let changes = diffLists (boardId summaryBoard) (boardLists summaryBoard) (boardLists newSummary)
           mapM_ (effectChange creds) changes
